71ef620383d5338317d58437fac85ce42b41514c
todor <tosheee@abv.bg>
2015-11-04 01:36:15 +0200

    Add class Words

---
+++ app/src/main/java/com/example/toshe/myapplication/Words.java
@@ -0,0 +1,15 @@
+package com.example.toshe.myapplication;
+
+/**
+ * Created by toshe on 4.11.2015 г..
+ */
+public class Words {
+
+    String wordBg, wordEn;
+    public Words(String wordBg, String wordEn){
+        this.wordBg = wordBg;
+        this.wordEn = wordEn;
+    }
+
+
+}
